5157b52bd3a2907ec3a82fdd85cea4a3cd93faf4
Charlene Chen <charlenechen@Charlenes-MacBook-Pro.local>
2026-04-17 09:31:19 +0100

    Add train pseudocode

---
+++ shell/train.sh
@@ -0,0 +1,4 @@
+# Collect data by running leaphand_node individually, motors disabled.
+# Save data in repo as data/ with cli input name
+# Reenable leaphand_node functions, stream position_node
+# Collect for 10 repeatss
