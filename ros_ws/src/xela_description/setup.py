2d461bca82de309823c4b57713b11bcfd3785650
mohammad <mohammad200h@hotmail.com>
2026-04-20 20:42:51 +0100

    added inital urdf for xela. mass is incorrect

--- ros_ws/src/xela_description/setup.py
+++ ros_ws/src/xela_description/setup.py
@@ -1,6 +1,34 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 package_name = 'xela_description'
+
+here = Path(__file__).resolve().parent
+
+
+def _data_files_under(rel_dir: str, install_subdir: str):
+    """
+    Collect all files under `rel_dir` into `share/<pkg>/<install_subdir>/...`.
+    """
+    src_root = here / rel_dir
+    out = []
+    if not src_root.exists():
+        return out
+    for root, _, files in os.walk(src_root):
+        if not files:
+            continue
+        root_path = Path(root)
+        rel = root_path.relative_to(src_root)
+        install_dir = str(Path("share") / package_name / install_subdir / rel)
+        # ament_python requires sources in data_files to be relative paths
+        rel_sources = [str((root_path / f).relative_to(here)) for f in files]
+        out.append((install_dir, rel_sources))
+    return out
+
 
 setup(
     name=package_name,
@@ -10,7 +38,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+        ('share/' + package_name, ['xela_description/robot.urdf']),
+    ]
+    + _data_files_under("launch", "launch")
+    + _data_files_under("xela_description/assets", "assets"),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
@@ -24,6 +55,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'visulize = xela_description.visulize_node:main',
         ],
     },
 )
+
